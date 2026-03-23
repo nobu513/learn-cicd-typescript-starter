e90d91725a94adf4719a03d71d17662e2afb96dc
nobu513 <nolun513@gmail.com>
2026-03-24 02:57:25 +0900

    add cd

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -13,20 +13,3 @@ export function getAPIKey(headers: IncomingHttpHeaders): string | null {
 
   return splitAuth[1];
 }
-
-import { describe, expect, test } from "vitest";
-
-const person = {
-  isActive: true,
-  age: 32,
-};
-
-describe("person", () => {
-  test("person is defined", () => {
-    expect(person).toBeDefined();
-  });
-
-  test("is active", () => {
-    expect(person.isActive).toBeTruthy();
-  });
-});
